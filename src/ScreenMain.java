04dbdf90c7cb80e53d8f738cc47eae5c8a1c2c30
EDSON JUNIOR <edsonmdcjunior1998@gmail.com>
2024-05-12 19:54:41 -0300

    Inicio do projeto.

---
+++ src/ScreenMain.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.io.IOException;
+
+public class ScreenMain {
+
+    public ScreenMain() {
+        JFrame screenMain = new JFrame("Treinamento de SQL");
+        screenMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screenMain.setLocationRelativeTo(null);
+        screenMain.setSize(800, 600);
+
+
+        JPanel panelMain = new JPanel();
+        panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.Y_AXIS));
+        panelMain.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // Criação do contentPanel
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        JLabel titletraining = new JLabel();
+        titletraining.setText("COMANDOS SQL");
+        titletraining.setFont(new Font("Arial", Font.BOLD, 60));
+        titletraining.setAlignmentX(Component.CENTER_ALIGNMENT);
+        contentPanel.add(titletraining);
+
+        // Adicionando JEditorPane para exibir HTML
+        JEditorPane editorPane = new JEditorPane();
+        editorPane.setEditable(false); // Não permitir edição
+        editorPane.setContentType("text/html"); // Definir tipo de conteúdo como HTML
+        editorPane.setPreferredSize(new Dimension(600, 9000)); // Definir tamanho preferido
+        contentPanel.add(editorPane);
+
+        // Carregar conteúdo HTML
+        try {
+            editorPane.setPage(getClass().getResource("comandossql.html")); // Substitua "comandos_sql.html" pelo nome do seu arquivo HTML
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Adicionando o contentPanel a um JScrollPane
+        JScrollPane scrollPane = new JScrollPane();
+        JViewport viewport = new JViewport();
+        viewport.setView(contentPanel);
+        scrollPane.setViewport(viewport);
+        panelMain.add(scrollPane);
+
+
+        screenMain.add(panelMain);
+        screenMain.setVisible(true);
+    }
+}
